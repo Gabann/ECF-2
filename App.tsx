7826893138f272ff06c6e9f668ef19ba73bb6ee3
Gaban <gabin.deboulogne@gmail.com>
2024-03-14 14:10:00 +0100

    Imported react native and react native navigation

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+export default function App(): React.JSX.Element {
+	return (
+		// <NavigationContainer>
+		// 	<Stack.Navigator
+		// 		screenOptions={{headerShown: false}}>
+		//
+		// 	</Stack.Navigator>
+		// </NavigationContainer>
+		<></>
+	)
+}
